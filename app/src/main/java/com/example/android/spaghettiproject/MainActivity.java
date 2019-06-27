18e6b94f9e1856b9d2e62c0aa2d3ca465e112991
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2019-06-27 16:00:15 -0400

    Add GroupsActivity, refactored sample button on MainActivity, created Intent from sample button to GroupsActivity

--- app/src/main/java/com/example/android/spaghettiproject/MainActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.android.spaghettiproject;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "com.example.android.spaghettiproject.extra.MESSAGE";
+    private static Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button_group);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fabAdd = findViewById(R.id.fab);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,
+//                        OrderActivity.class);
+//                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+//                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return super.onCreateOptionsMenu(menu);
+    }
 
+
+    public void goToGroup(View view) {
+        Intent intent = new Intent(this, GroupsActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, button.getText().toString());
+        startActivity(intent);
     }
 }
