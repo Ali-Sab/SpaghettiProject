6b46963b0af7b9e2f4ac8e32507e6b94fd25a927
Stephen Berry <stephen.p.berry@hotmail.ca>
2019-07-01 21:55:36 -0400

    added option to flip between login and registering accounts

--- app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.spaghettiproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText user;
+    private EditText password;
+    private Button login;
+    private TextView loginText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user = (EditText)findViewById(R.id.editTextUser);
+        password = (EditText)findViewById(R.id.editTextPassword);
+        login = (Button) findViewById(R.id.btnLogin);
+        loginText = (TextView)findViewById(R.id.textViewLogin);
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profileIntent = new Intent(LoginActivity.this, ProfileActivity.class);
+                startActivity(profileIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/spaghettiproject/ProfileActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/ProfileActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.spaghettiproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -12,6 +16,7 @@ public class ProfileActivity extends AppCompatActivity {
     private EditText pass1;
     private EditText pass2;
     private Button setup;
+    private TextView login;
     private String password;
 
     @Override
@@ -22,6 +27,16 @@ public class ProfileActivity extends AppCompatActivity {
         email = (EditText)findViewById(R.id.editTextEmail);
         pass1 = (EditText)findViewById(R.id.editTextPassword1);
         pass2 = (EditText)findViewById(R.id.editTextPassword2);
+        login = (TextView)findViewById(R.id.textViewLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
         setup = (Button)findViewById(R.id.btnSetup);
 
         setup.setOnClickListener(new View.OnClickListener() {
