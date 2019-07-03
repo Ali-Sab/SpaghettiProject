af22f420555fab2f917d580ab0b70c3192a19515
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2019-07-03 17:39:44 -0400

    Auto set title of ListsActivity with selected group

--- app/src/main/java/com/example/android/spaghettiproject/GroupsActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/GroupsActivity.java
@@ -1,29 +1,49 @@
 package com.example.android.spaghettiproject;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class GroupsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.android.spaghettiproject.extra.MESSAGE";
+    private static Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        button = (Button) findViewById(R.id.button_group);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabAdd = findViewById(R.id.fab);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Intent intent = new Intent(MainActivity.this,
+//                        OrderActivity.class);
+//                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+//                startActivity(intent);
             }
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void goToLists(View view) {
+        Intent intent = new Intent(GroupsActivity.this, ListsActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, button.getText().toString());
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        Intent intent = new Intent(LoginActivity.this, GroupsActivity.class);
         startActivity(intent);
     }
 
