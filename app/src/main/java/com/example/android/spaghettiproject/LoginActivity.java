2559f5d5a09c7b3076dff34beb69fbb7681165e2
Stephen Berry <stephen.p.berry@hotmail.ca>
2019-07-04 15:14:51 -0400

    fixing login and profile layout, as well as added checkbox to keep logged in (no current functionality)

--- app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
+++ app/src/main/java/com/example/android/spaghettiproject/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.spaghettiproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,21 +28,22 @@ public class LoginActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.btnLogin);
         loginText = (TextView)findViewById(R.id.textViewLogin);
 
-//        loginText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent profileIntent = new Intent(LoginActivity.this, ProfileActivity.class);
-//                startActivity(profileIntent);
-//            }
-//        });
+
 
     }
 
     public void onClick(View view) {
+        //Run below only if user account and password matches
         Intent intent = new Intent(LoginActivity.this, GroupsActivity.class);
         startActivity(intent);
     }
 
     public void goToProfile(View view) {
+        Intent profileIntent = new Intent(LoginActivity.this, ProfileActivity.class);
+        startActivity(profileIntent);
+    }
+
+    public void keepLoggedIn(View view){
+
     }
 }
