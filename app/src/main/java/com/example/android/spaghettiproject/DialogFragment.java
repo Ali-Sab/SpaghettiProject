f16bd5527641465d673735bd22014fc41c893565
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2019-09-16 23:29:10 -0400

    Improve LoginActivity layout

--- app/src/main/java/com/example/android/spaghettiproject/DialogFragment.java
+++ app/src/main/java/com/example/android/spaghettiproject/DialogFragment.java
@@ -52,19 +52,19 @@ public class DialogFragment extends Fragment implements DialogInterface.OnCancel
         return fragment;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_alert_dialog, container, false);
-        //Dialog dialog = new AlertDialog();
-        //dialog.getWindow().getAttributes().windowAnimations = R.style.AppTheme;
-
-        //Find the +1 button
-
-
-        return view;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        //View view = inflater.inflate(R.layout.fragment_alert_dialog, container, false);
+//        //Dialog dialog = new AlertDialog();
+//        //dialog.getWindow().getAttributes().windowAnimations = R.style.AppTheme;
+//
+//        //Find the +1 button
+//
+//
+//        //return view;
+//    }
 
     @Override
     public void onResume() {
